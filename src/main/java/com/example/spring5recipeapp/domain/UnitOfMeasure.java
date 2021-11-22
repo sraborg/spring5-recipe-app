65e4c57f65b6b89f61ac6fd51add1c8efa240182
Sean Raborg <sean.raborg@gmail.com>
2021-11-21 19:50:24 -0700

    UnitOfMeasure model and configure OneToOne JPA relationship with Ingredient as a uni-directional relationship. Closes #5.

---
+++ src/main/java/com/example/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,31 @@
+package com.example.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
