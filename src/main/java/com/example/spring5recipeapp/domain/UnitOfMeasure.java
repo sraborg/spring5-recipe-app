5c5ea77a9a8cb6820a044c41b3bdf43704415694
Sean Raborg <sean.raborg@gmail.com>
2021-11-23 11:29:55 -0700

    Added recipes on startup and displayed on index. Closes #11. Closes #12.

--- src/main/java/com/example/spring5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/com/example/spring5recipeapp/domain/UnitOfMeasure.java
@@ -11,7 +11,7 @@ public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String Description;
+    private String description;
 
     public Long getId() {
         return id;
@@ -22,10 +22,10 @@ public class UnitOfMeasure {
     }
 
     public String getDescription() {
-        return Description;
+        return description;
     }
 
     public void setDescription(String description) {
-        Description = description;
+        this.description = description;
     }
 }
