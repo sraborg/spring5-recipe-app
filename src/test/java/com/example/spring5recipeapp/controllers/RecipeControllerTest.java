26293cc5e66638eb3d61c0d345b497c0dcef9075
Sean Raborg <sean.raborg@gmail.com>
2021-11-26 10:12:33 -0700

    Added Tests and method stubs for RecipeController. Closes #22.

---
+++ src/test/java/com/example/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -0,0 +1,50 @@
+package com.example.spring5recipeapp.controllers;
+
+import com.example.spring5recipeapp.domain.Recipe;
+import com.example.spring5recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class RecipeControllerTest {
+
+    @Mock
+    private RecipeService recipeService;
+
+    private RecipeController recipeController;
+
+    private MockMvc mockMvc;
+
+    private final Recipe recipe;
+    private final Long recipe_id = 1L;
+    {
+        recipe = new Recipe();
+        recipe.setId(recipe_id);
+    }
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    void showById() throws Exception {
+
+        given(recipeService.findById(recipe_id)).willReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/recipe/show"));
+    }
+}
