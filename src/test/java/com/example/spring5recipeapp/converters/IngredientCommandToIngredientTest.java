490df0973773b8999e3b6b7e0cd30da841190b2c
Sean Raborg <sean.raborg@gmail.com>
2021-11-26 17:02:52 -0700

    Refactored Ingredient command converter test to use mocks. Closes #32.

--- src/test/java/com/example/spring5recipeapp/converters/IngredientCommandToIngredientTest.java
+++ src/test/java/com/example/spring5recipeapp/converters/IngredientCommandToIngredientTest.java
@@ -3,51 +3,59 @@ package com.example.spring5recipeapp.converters;
 import com.example.spring5recipeapp.command.IngredientCommand;
 import com.example.spring5recipeapp.command.UnitOfMeasureCommand;
 import com.example.spring5recipeapp.domain.Ingredient;
+import com.example.spring5recipeapp.domain.UnitOfMeasure;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
+@ExtendWith(MockitoExtension.class)
 class IngredientCommandToIngredientTest {
 
-    private final UnitOfMeasureCommandToUnitOfMeasure uomCommandConverter = new UnitOfMeasureCommandToUnitOfMeasure();
-    private final IngredientCommandToIngredient converter = new IngredientCommandToIngredient(uomCommandConverter);
+    @Mock
+    private UnitOfMeasureCommandToUnitOfMeasure uomCommandConverter;
 
+    @InjectMocks
+    private IngredientCommandToIngredient converter;
+    private final Ingredient ingredient = new Ingredient();
     private final Long INGREDIENT_ID = 1L;
     private final String INGREDIENT_DESCRIPTION = "ingredient description";
     private final BigDecimal AMOUNT = new BigDecimal(20.0);
-    private final UnitOfMeasureCommand UNIT_OF_MEASURE = new UnitOfMeasureCommand();
-    private final Long UOM_ID = 2L;
-    private final String UOM_DESCRIPTION = "uom description";
-    //private Recipe recipe;
-
-    {
-        UNIT_OF_MEASURE.setId(UOM_ID);
-        UNIT_OF_MEASURE.setDescription(UOM_DESCRIPTION);
-    }
+    private final UnitOfMeasureCommand unitOfMeasureCommand = new UnitOfMeasureCommand();
+    private final UnitOfMeasure UNIT_OF_MEASURE = new UnitOfMeasure();
 
     @Test
     void convert() {
 
-        IngredientCommand ingredientCommand = new IngredientCommand();
+        // Given
+        final IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(INGREDIENT_ID);
         ingredientCommand.setDescription(INGREDIENT_DESCRIPTION);
         ingredientCommand.setAmount(AMOUNT);
-        ingredientCommand.setUom(UNIT_OF_MEASURE);
+        ingredientCommand.setUom(unitOfMeasureCommand);
 
+        given(uomCommandConverter.convert(any(UnitOfMeasureCommand.class))).willReturn(UNIT_OF_MEASURE);
+
+        // When
         Ingredient ingredient = converter.convert(ingredientCommand);
 
+        // Then
+        then(uomCommandConverter).should().convert(unitOfMeasureCommand);
         assertNotNull(ingredient);
         assertThat(ingredient.getId()).isEqualTo(INGREDIENT_ID);
         assertThat(ingredient.getDescription()).isEqualTo(INGREDIENT_DESCRIPTION);
         assertThat(ingredient.getAmount()).isEqualTo(AMOUNT);
-
-        // UOM
-        assertThat(ingredient.getUom().getId()).isEqualTo(UOM_ID);
-        assertThat(ingredient.getUom().getDescription()).isEqualTo(UOM_DESCRIPTION);
+        assertThat(ingredient.getUom()).isEqualTo(UNIT_OF_MEASURE);
 
     }
 
--- src/test/java/com/example/spring5recipeapp/converters/IngredientToIngredientCommandTest.java
+++ src/test/java/com/example/spring5recipeapp/converters/IngredientToIngredientCommandTest.java
@@ -1,53 +1,62 @@
 package com.example.spring5recipeapp.converters;
 
 import com.example.spring5recipeapp.command.IngredientCommand;
+import com.example.spring5recipeapp.command.UnitOfMeasureCommand;
 import com.example.spring5recipeapp.domain.Ingredient;
 import com.example.spring5recipeapp.domain.UnitOfMeasure;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
+@ExtendWith(MockitoExtension.class)
 class IngredientToIngredientCommandTest {
 
-    private final UnitOfMeasureToUnitOfMeasureCommand UomConverter = new UnitOfMeasureToUnitOfMeasureCommand();
-    private final IngredientToIngredientCommand converter = new IngredientToIngredientCommand(UomConverter);
+    @Mock
+    private UnitOfMeasureToUnitOfMeasureCommand UomConverter;
+
+    @InjectMocks
+    private IngredientToIngredientCommand converter;
 
     private final Long INGREDIENT_ID = 1L;
     private final String INGREDIENT_DESCRIPTION = "description";
     private final BigDecimal AMOUNT = new BigDecimal(20.0);
     private final UnitOfMeasure UNIT_OF_MEASURE = new UnitOfMeasure();
-    private final Long UOM_ID = 2L;
-    private final String UOM_DESCRIPTION = "uom_description";
-    //private Recipe RECIPE = new Recipe();
-    {
-        UNIT_OF_MEASURE.setId(UOM_ID);
-        UNIT_OF_MEASURE.setDescription(UOM_DESCRIPTION);
-    }
+    private final UnitOfMeasureCommand UNIT_OF_MEASURE_COMMAND = new UnitOfMeasureCommand();
 
     @Test
     void convert() {
+
+        // Given
         Ingredient ingredient = new Ingredient();
         ingredient.setId(INGREDIENT_ID);
         ingredient.setDescription(INGREDIENT_DESCRIPTION);
         ingredient.setAmount(AMOUNT);
         ingredient.setUom(UNIT_OF_MEASURE);
+        given(UomConverter.convert(UNIT_OF_MEASURE)).willReturn(UNIT_OF_MEASURE_COMMAND);
+
+
         //ingredient.setRecipe(RECIPE);
 
+        // When
         IngredientCommand command = converter.convert(ingredient);
 
+        // Then
+        then(UomConverter).should().convert(UNIT_OF_MEASURE);
         assertNotNull(command);
         assertThat(command.getId()).isEqualTo(INGREDIENT_ID);
         assertThat(command.getDescription()).isEqualTo(INGREDIENT_DESCRIPTION);
         assertThat(command.getAmount()).isEqualTo(AMOUNT);
 
-        // TEST UOM
-        assertThat(command.getUom().getId()).isEqualTo(UOM_ID);
-        assertThat(command.getUom().getDescription()).isEqualTo(UOM_DESCRIPTION);
-        //assertThat(command.getRecipe()).isEqualTo(RECIPE);
     }
 
     @Test
